f6f3c3cee6ebc788a41c236aafe03b7e3da4b6f2
Juan Pablo Miranda <jpablomiranda.21@gmail.com>
2023-07-14 20:26:18 -0300

    realizamos la conexion a la base de datos

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import path from "path";
+import 'dotenv/config';
+import './src/database/dbConnection';
 
 //usar un puerto
 const app = express();
@@ -15,7 +17,7 @@ app.listen(app.get('port'), ()=>{
 app.use(cors()); // permitir conexiones remotas
 app.use(express.json()) // permitir a mi aplicacion recibir objetos de tipo json en los request
 app.use(morgan('dev')); // me muestra en la consola informacion extra de las solicitudes GET, POST, PUT, etc.
-console.log(__dirname); // la variable __dirname siempre devuelve la ruta donde esta alojado el proyecto
+// console.log(__dirname); // la variable __dirname siempre devuelve la ruta donde esta alojado el proyecto
 app.use(express.static(path.join(__dirname, '/public'))); // esto nos permite poder ejecutar los archivos estaticos de mi proyuecto en la ruta raiz de mi backend ej: http://localhost:4000
 // http://localhost:4000
 
