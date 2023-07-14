7fa0c81c1b5361537550641853ce1d21d0f6961e
Juan Pablo Miranda <jpablomiranda.21@gmail.com>
2023-07-14 18:35:46 -0300

    se terminaron las configuraciones iniciales

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
+import path from "path";
 
 //usar un puerto
 const app = express();
@@ -11,5 +13,11 @@ app.listen(app.get('port'), ()=>{
 
 //middlewares: funciones que se ejecutan antes de las rutas
 app.use(cors()); // permitir conexiones remotas
+app.use(express.json()) // permitir a mi aplicacion recibir objetos de tipo json en los request
+app.use(morgan('dev')); // me muestra en la consola informacion extra de las solicitudes GET, POST, PUT, etc.
+console.log(__dirname); // la variable __dirname siempre devuelve la ruta donde esta alojado el proyecto
+app.use(express.static(path.join(__dirname, '/public'))); // esto nos permite poder ejecutar los archivos estaticos de mi proyuecto en la ruta raiz de mi backend ej: http://localhost:4000
+// http://localhost:4000
+
 
 //rutas
